0b3ae046991b406ec1eff58114545321ccefcfad
2klab <github@2klab.com>
2019-10-01 21:30:27 +0100

    First. Second address to be updated.

---
+++ GetDistanceBtwAddresses/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace GetDistanceBtwAddresses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(167, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(511, 74);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(167, 83);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(511, 74);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(167, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(511, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(681, 323);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ravibioshop@gmail.com";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(167, 243);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(511, 77);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Address1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 462);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GetDistanceBtwAddresses/Form1.cs
@@ -0,0 +1,96 @@
+﻿using Nominatim.API.Geocoders;
+using Nominatim.API.Models;
+using System;
+using System.Device.Location;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetDistanceBtwAddresses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public GeocodeResponse GetGeocode(string address)
+        {
+            ForwardGeocoder fg = new ForwardGeocoder();
+            ForwardGeocodeRequest fgr = new ForwardGeocodeRequest();
+            //essayer streetaddress
+            Task<GeocodeResponse[]> ar = fg.Geocode(new ForwardGeocodeRequest
+            {
+                queryString = address,
+                DedupeResults = true,
+                BreakdownAddressElements = true,
+                ShowExtraTags = true,
+                ShowAlternativeNames = true,
+                ShowGeoJSON = true
+            });
+            ar.Wait();
+
+            GeocodeResponse gr;
+            if (ar.Result.Length == 0)
+                return null;
+
+            gr = ar.Result[0];
+            return gr;
+        }
+
+        public Boolean IsAddressValid(string address)
+        {
+            GeocodeResponse gr = GetGeocode(address);
+            if (gr is null)
+                return false;
+            else
+                return true;
+        }
+
+        public double GetDistanceInMeter(GeocodeResponse gr1, GeocodeResponse gr2)
+        {
+            GeoCoordinate pin1 = new GeoCoordinate(gr1.Latitude, gr1.Longitude);
+            GeoCoordinate pin2 = new GeoCoordinate(gr2.Latitude, gr2.Longitude);
+
+            double distanceBetween = pin1.GetDistanceTo(pin2);
+            return Math.Round(distanceBetween, 0);
+        }
+
+        public Boolean IsWithinDistanceInMeter(GeocodeResponse gr1, GeocodeResponse gr2, double distance = 500)
+        {
+            double dist = GetDistanceInMeter(gr1, gr2);
+            if (dist <= distance)
+                return true;
+            else
+                return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string address1 = "57 avenue de l'arche, 92400,Courbevoie, France";
+            GeocodeResponse gr1 = GetGeocode(address1);
+
+
+            string address2 = "4 bd montparnasse, 75014 Paris, France";
+            GeocodeResponse gr2 = GetGeocode(address2);
+
+            double distanceBetween = GetDistanceInMeter(gr1, gr2);
+
+            string no1 = gr1.Address.Pedestrian + gr1.Address.Road;
+            string no2 = gr2.Address.Pedestrian + gr2.Address.Road;
+            textBox4.Text += " From:" + gr1.Address.HouseNumber + " " + no1 + " " + gr1.Address.PostCode + " " + gr1.Address.Town;
+            textBox4.Text += " To:" + gr2.Address.HouseNumber + " " + no2 + " " + gr2.Address.PostCode + " " + gr2.Address.Town;
+            textBox4.Text += " lat=" + gr1.Latitude.ToString() + "; lon=" + gr1.Longitude.ToString();
+            textBox4.Text += ". distance=" + distanceBetween + " m";
+            textBox4.Text += ". Within 500m? " + IsWithinDistanceInMeter(gr1, gr2, 500);
+            textBox4.Text += ". Is valid address ? " + IsAddressValid(address1);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
